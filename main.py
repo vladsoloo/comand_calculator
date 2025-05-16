8929569eede3d7576fc4b1c86125dcf70cc77c78
brother <hacker@gmail.com>
2025-05-16 11:42:31 +0300

    Кирилл

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 def main():
-    pass
+    print("Простой калькулятор")
+    print("Доступные операции: +, -, *, /")
+    
+    while True:
+        try:
+ 
+            num1 = float(input("Введите первое число: "))
+            operator = input("Введите оператор (+, -, *, /): ")
+            num2 = float(input("Введите второе число: "))
+            
+ 
+            if operator == '+':
+                result = num1 + num2
+            elif operator == '-':
+                result = num1 - num2
+            elif operator == '*':
+                result = num1 * num2
+            elif operator == '/':
+                if num2 == 0:
+                    print("Ошибка: Деление на ноль!")
+                    continue
+                result = num1 / num2
+            else:
+                print("Ошибка: Неверный оператор!")
+                continue
+            
+
+            print(f"Результат: {num1} {operator} {num2} = {result}")
 
 
-if __name__ == '__main__':
+            again = input("Хотите выполнить еще одно вычисление? (да/нет): ")
+            if again.lower() != 'да':
+                print("Спасибо за использование калькулятора!")
+                break
+                
+        except ValueError:
+            print("Ошибка: Пожалуйста, введите корректное число!")
+            continue
+
+if __name__ == 'main':
     main()

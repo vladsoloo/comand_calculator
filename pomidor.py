f7d5f72296b5f3f8d989ad9d7821b84cab52d343
FedorUltraz <zhfedor33@gmail.com>
2025-05-16 13:54:00 +0300

    соломахин

--- pomidor.py
+++ pomidor.py
@@ -1,12 +1,12 @@
 import os
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from dotenv import find_dotenv, load_dotenv
 from loguru import logger
 
 # Настройка Loguru
-logger.add("spy_bot.log", format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}", level="INFO")
+logger.add("spy_bot.log", format="{time:YYYY-MM-DD HH:mm:ss} | {level} | \
+           {message}", level="INFO")
 
 # Загрузка конфига
 load_dotenv(find_dotenv())
@@ -17,6 +17,8 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 # При любом сообщении бот логирует данные и шлет их админу
+
+
 @dp.message()
 async def spy_on_user(message: Message):
     user = message.from_user

104bb63b1517915d81e92cd56e7a933a86d61b22
FedorUltraz <zhfedor33@gmail.com>
2025-05-16 13:23:30 +0300

    соломахин

---
+++ heandler.py
@@ -0,0 +1,20 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Привет! Я ваш бот. Чем могу помочь?')
+
+
+def main():
+
+    application = ApplicationBuilder().token
+    ('8142723527:AAGU7HtE71u6Wu0Mf-8m22Gg_Ixn2c5ETaI').build()
+
+    application.add_handler(CommandHandler('start', start))
+
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()

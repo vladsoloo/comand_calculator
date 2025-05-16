3a380473e2fd33953c9df7b845ff8c6a4b940eab
Luch3r <luchkin_yrok@mail.ru>
2025-05-16 14:10:57 +0300

    main is ready

--- main.py
+++ main.py
@@ -1,42 +1,33 @@
-def main():
-    print("Простой калькулятор")
-    print("Доступные операции: +, -, *, /")
-    
-    while True:
-        try:
- 
-            num1 = float(input("Введите первое число: "))
-            operator = input("Введите оператор (+, -, *, /): ")
-            num2 = float(input("Введите второе число: "))
-            
- 
-            if operator == '+':
-                result = num1 + num2
-            elif operator == '-':
-                result = num1 - num2
-            elif operator == '*':
-                result = num1 * num2
-            elif operator == '/':
-                if num2 == 0:
-                    print("Ошибка: Деление на ноль!")
-                    continue
-                result = num1 / num2
-            else:
-                print("Ошибка: Неверный оператор!")
-                continue
-            
+import asyncio
+import os
+from loguru import logger
+from dotenv import load_dotenv, find_dotenv
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from chat import run_bot
 
-            print(f"Результат: {num1} {operator} {num2} = {result}")
+load_dotenv(find_dotenv())
+TOKEN = os.getenv("TOKEN")
 
 
-            again = input("Хотите выполнить еще одно вычисление? (да/нет): ")
-            if again.lower() != 'да':
-                print("Спасибо за использование калькулятора!")
-                break
-                
-        except ValueError:
-            print("Ошибка: Пожалуйста, введите корректное число!")
-            continue
+async def main():
+    logger.add("file.log",
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation="3 days",
+               backtrace=True,
+               diagnose=True)
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+    dp = Dispatcher()
 
-if __name__ == 'main':
-    main()
+    logger.info("Бот запущен")
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+        logger.info("Бот остановлен")
+
+    run_bot()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())

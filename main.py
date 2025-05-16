8f4e805d50bdbd2d3ed2bd6ea1be5e2bc02e677a
FedorUltraz <zhfedor33@gmail.com>
2025-05-16 13:42:26 +0300

    исправил ошибки, соломахин

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 def main():
     print("Простой калькулятор")
     print("Доступные операции: +, -, *, /")
-    
+
     while True:
         try:
- 
+
             num1 = float(input("Введите первое число: "))
             operator = input("Введите оператор (+, -, *, /): ")
             num2 = float(input("Введите второе число: "))
-            
- 
+
             if operator == '+':
                 result = num1 + num2
             elif operator == '-':
@@ -24,19 +23,18 @@ def main():
             else:
                 print("Ошибка: Неверный оператор!")
                 continue
-            
 
             print(f"Результат: {num1} {operator} {num2} = {result}")
-
 
             again = input("Хотите выполнить еще одно вычисление? (да/нет): ")
             if again.lower() != 'да':
                 print("Спасибо за использование калькулятора!")
                 break
-                
+
         except ValueError:
             print("Ошибка: Пожалуйста, введите корректное число!")
             continue
+
 
 if __name__ == 'main':
     main()

c4c7f7fd05f0bdc96bdfaa9abaa3d4b34c2a7ea0
FedorUltraz <zhfedor33@gmail.com>
2025-05-20 15:12:54 +0300

    FIXED

--- main.py
+++ main.py
@@ -3,31 +3,24 @@ import os
 from loguru import logger
 from dotenv import load_dotenv, find_dotenv
 from aiogram import Bot, Dispatcher
-from chat import numbers
-
 
 dp = Dispatcher()
-
 load_dotenv(find_dotenv())
 TOKEN = os.getenv("TOKEN")
 
 
-async def main1():
-    logger.add("file.log",
-               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-               rotation="3 days",
+async def main1() -> None:
+    logger.add('file.log',
+               format='{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}',
+               rotation='3 days',
                backtrace=True,
                diagnose=True)
-
     bot = Bot(token=os.getenv('TOKEN'))
-    dp = Dispatcher()
     try:
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
         logger.info("Бот остановлен")
-
-    numbers(dp)
 
 
 if __name__ == '__main1__':
